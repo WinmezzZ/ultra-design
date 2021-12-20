314cab2bb2c4a76a25ffcdfaaebf3521dc90cee0
Winme <winme0308@gmail.com>
2021-12-21 02:56:28 +0800

    complete modal compoent

--- components/modal/modal-style.ts
+++ components/modal/modal-style.ts
@@ -1,39 +1,102 @@
 import { css } from '@emotion/react';
+import { ConfigCommonOptions } from '../config-provider';
+import { ModalProps } from './modal';
 
-export const modalWrapperStyle = css`
-  position: fixed;
-  inset: 0;
-  z-index: 1000;
-  .ultra-modal {
-    position: relative;
-    top: 100px;
-    width: 50%;
-    margin: 0 auto;
-    background-color: #fff;
-    padding: 20px;
-    border-radius: 4px;
-  }
+export interface ModalCSSProps extends ModalProps, ConfigCommonOptions {}
 
-  /* modal wrapper transition */
+export const modalWrapperStyle = (props: ModalCSSProps) => {
+  const { center, top, width, theme } = props;
+  const { radius } = theme.style;
 
-  &.ultra-modal-wrapper-enter {
-    transform: scale(0);
-    opacity: 0;
-  }
-  &.ultra-modal-wrapper-enter-active {
-    transform: scale(1);
-    opacity: 1;
-    transition-property: transform opacity;
-    transition: 300ms;
-  }
-  &.ultra-modal-wrapper-exit {
-    opacity: 1;
-    transform: scale(1);
-  }
-  &.ultra-modal-wrapper-exit-active {
-    opacity: 0;
-    transform: scale(0);
-    transition-property: transform opacity;
-    transition: 300ms;
-  }
-`;
+  return css`
+    position: fixed;
+    inset: 0;
+    z-index: 1000;
+    * {
+      box-sizing: border-box;
+    }
+    ${center &&
+    css`
+      text-align: center;
+      &::before {
+        display: inline-block;
+        width: 0;
+        height: 100%;
+        vertical-align: middle;
+        content: '';
+      }
+    `}
+    .ultra-modal {
+      position: relative;
+      width: ${width};
+      max-width: 90%;
+      background-color: #fff;
+      padding: 20px;
+      border-radius: ${radius}px;
+      margin: 0 auto;
+      overflow: auto;
+      ${center
+        ? css`
+            ${top &&
+            css`
+              top: ${top};
+            `}
+            display: inline-block;
+            vertical-align: middle;
+            text-align: left;
+          `
+        : css`
+            top: ${top || '10vh'};
+          `}
+      font-size: 14px;
+      .ultra-modal-header {
+        display: flex;
+        align-items: center;
+        flex-direction: row-reverse;
+        &__title {
+          flex: 1;
+          margin: 0;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          font-weight: 400;
+          font-size: 16px;
+        }
+
+        &__close {
+          margin-left: 10px;
+          cursor: pointer;
+        }
+      }
+      .ultra-modal-body {
+        padding: 20px 0;
+      }
+      .ultra-modal-footer {
+        text-align: right;
+      }
+    }
+
+    /* modal wrapper transition */
+
+    &.ultra-modal-wrapper-enter {
+      transform: scale(0);
+      opacity: 0;
+    }
+    &.ultra-modal-wrapper-enter-active {
+      transform: scale(1);
+      opacity: 1;
+      transition-property: transform opacity;
+      transition: 300ms;
+    }
+    &.ultra-modal-wrapper-exit {
+      opacity: 1;
+      transform: scale(1);
+    }
+    &.ultra-modal-wrapper-exit-active {
+      opacity: 0;
+      transform: scale(0);
+      transition-property: transform opacity;
+      transition: 300ms;
+    }
+  `;
+};
--- components/modal/modal.tsx
+++ components/modal/modal.tsx
@@ -3,20 +3,103 @@ import { createPortal } from 'react-dom';
 import { CSSTransition } from 'react-transition-group';
 import Overlay from '../overlay';
 import { modalWrapperStyle } from './modal-style';
+import Button, { ButtonProps } from '../button';
+import { Close } from '@icon-park/react';
+import { useConfigContext } from '../config-provider/useConfigContext';
+
+type ModalButtonProps = Pick<ButtonProps, 'disabled' | 'loading' | 'type' | 'children'>;
 
 export interface ModalProps {
+  /**
+   * @description.zh-CN 对话框标题
+   * @description.en-US modal's title
+   */
+  title?: string;
+  /**
+   * @description.zh-CN 是否显示
+   * @description.en-US modal's visible
+   * @default false
+   */
   visible: boolean;
+  /**
+   * @description.zh-CN 对话框关闭时会触发此方法，比如点击取消按钮、点击关闭图标、按下 esc 键的时候
+   * @description.en-US trigger when modal closed, like clicked cancel button, close icon and pressed escape key
+   */
   onClose?: () => void;
+  /**
+   * @description.zh-CN 点击确定按钮时会触发此方法
+   * @description.en-US trigger when clicked confirm button
+   */
+  onOk?: () => void;
+  /**
+   * @description.zh-CN 关闭前执行的方法，如果此方法返回 false，那么将会阻止对话框关闭，支持异步。用途：比如关闭前确认
+   * @description.en-US do something before modal close, return `false` will stop close modal, support sync function
+   */
+  beforeClose?: () => Promise<any>;
+  /**
+   * @description.zh-CN 对话框相对浏览器窗口的 top 属性
+   * @description.en-US the top attribute of the modal relative to the browser window
+   * @default '10vh'
+   */
+  top?: string;
+  /**
+   * @description.zh-CN 对话框的宽度，除了百分比，也可以设置为固定的 px
+   * @description.en-US The width of the modal, in addition to the percentage, can also be set to a fixed px
+   * @default '50%'
+   */
+  width?: string;
+  /**
+   * @description.zh-CN 对话框是否垂直居中
+   * @description.en-US set modal vertical center
+   * @default false
+   */
+  center?: boolean;
+  /**
+   * @description.zh-CN 是否可以通过键盘的 esc 按键关闭对话框
+   * @description.en-US close modal when press escape key on the keyboard
+   * @default true
+   */
+  keyboard?: boolean;
+  /**
+   * @description.zh-CN 隐藏右上角关闭图标
+   * @description.en-US hide upper right close icon
+   * @default false
+   */
+  hideClose?: boolean;
+  /**
+   * @description.zh-CN 确定按钮的属性，支持 'disabled', 'loading', 'type', 'children'，设置为 `undefined` 将隐藏确认按钮
+   * @description.en-US conform button props, support 'disabled', 'loading', 'type', 'children', set to `undefined` will hide confirm button
+   * @default { children: 'Ok', type: 'primary' }
+   */
+  conformButton?: ModalButtonProps;
+  /**
+   * @description.zh-CN 取消按钮的属性，支持 'disabled', 'loading', 'type', 'children'，设置为 `undefined` 将隐藏取消按钮
+   * @description.en-US cancel button props, support 'disabled', 'loading', 'type', 'children', set to `undefined` will hide cancel button
+   * @default { children: 'Cancel' }
+   */
+  cancelButton?: ModalButtonProps;
 }
 
 const Modal: FC<ModalProps> = props => {
-  const { visible, onClose, children } = props;
+  const { title, visible, onClose, onOk, conformButton, cancelButton, keyboard, beforeClose, hideClose, children } =
+    props;
+  const configContext = useConfigContext();
+  const cssProps = { ...configContext, ...props };
+  const conformButtonProps: ModalButtonProps = Object.assign({}, { type: 'primary', children: '确定' }, conformButton);
+  const cancelBtnProps: ModalButtonProps = Object.assign({}, { children: '取消' }, cancelButton);
 
-  const closeHandler = () => {
+  const closeHandler = async () => {
+    if (beforeClose) {
+      const cancelClose = (await beforeClose()) === false;
+      // quite close when beforeClose return false
+
+      if (cancelClose) return;
+    }
     onClose?.();
   };
 
   useEffect(() => {
+    if (!keyboard) return;
     const listenEscKeyDown = (e: KeyboardEvent) => {
       if (e.code === 'Escape') {
         closeHandler();
@@ -36,8 +119,20 @@ const Modal: FC<ModalProps> = props => {
         <div>
           <Overlay visible={visible} timeout={300} />
           <CSSTransition in={visible} unmountOnExit timeout={300} classNames="ultra-modal-wrapper">
-            <div onClick={closeHandler} css={modalWrapperStyle} className="ultra-modal-wrapper">
-              <div className="ultra-modal">{children}</div>
+            <div css={modalWrapperStyle(cssProps)} className="ultra-modal-wrapper">
+              <div className="ultra-modal">
+                <div className="ultra-modal-header">
+                  {!hideClose && <Close className="ultra-modal-header__close" onClick={closeHandler} />}
+                  <h4 className="ultra-modal-header__title">{title}</h4>
+                </div>
+                <div className="ultra-modal-body">{children}</div>
+                {!(!conformButton && !cancelButton) && (
+                  <div className="ultra-modal-footer">
+                    <Button {...conformButtonProps} onClick={onOk} />
+                    <Button {...cancelBtnProps} onClick={closeHandler} />
+                  </div>
+                )}
+              </div>
             </div>
           </CSSTransition>
         </div>,
@@ -45,6 +140,12 @@ const Modal: FC<ModalProps> = props => {
       )}
     </>
   );
+};
+
+Modal.defaultProps = {
+  visible: false,
+  keyboard: true,
+  width: '50%',
 };
 
 export default Modal;
