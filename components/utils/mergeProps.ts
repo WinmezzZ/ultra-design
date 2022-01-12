132652aad76bfd09b088ede5979b45cbdf9f9307
Winme <winme0308@gmail.com>
2022-01-12 16:17:40 +0800

    chore: add mergeProps helper function

---
+++ components/utils/mergeProps.ts
@@ -0,0 +1,25 @@
+import { assign, assignWith, isUndefined } from 'lodash-es';
+import { ConfigContextOptions } from '../config-provider';
+import { useConfigContext } from '../config-provider/useConfigContext';
+
+export function mergeProps<A, B>(a: A, b: B): B & A;
+export function mergeProps<A, B, C>(a: A, b: B, c: C): C & B & A;
+export function mergeProps(...items: any[]) {
+  function customizer(objValue: any, srcValue: any) {
+    return isUndefined(srcValue) ? objValue : srcValue;
+  }
+
+  let ret = assign({}, items[0]);
+
+  for (let i = 1; i < items.length; i++) {
+    ret = assignWith(ret, items[i], customizer);
+  }
+
+  return ret;
+}
+
+export function useMergeProps<A, B>(defaultProps: A, props: B): B & A & ConfigContextOptions {
+  const configContext = useConfigContext();
+
+  return mergeProps(defaultProps, configContext, props);
+}
