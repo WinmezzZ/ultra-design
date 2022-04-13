e6b3d2b5b37657ed7d9707f7d621b20649c10820
Winme <winme0308@gmail.com>
2022-04-13 12:13:02 +0800

    fix: #1

--- .dumi/theme/layout.tsx
+++ .dumi/theme/layout.tsx
@@ -2,24 +2,14 @@ import React, { FC, useMemo } from 'react';
 import Layout from 'dumi-theme-default/src/layout';
 import { ConfigProvider } from '../../components';
 import { usePrefersColor } from 'dumi/theme';
-
-type ThemeMode = 'dark' | 'light';
-
-const themes: ThemeMode[] = ['dark', 'light'];
+import { useColorScheme } from '@winme/react-hooks';
 
 const CustomLayout: FC<any> = ({ children, ...props }) => {
   const [color] = usePrefersColor();
-
-  const themeMode = useMemo(() => {
-    return themes.includes(color as ThemeMode)
-    ? color as ThemeMode
-    : (document.querySelector('html').getAttribute('data-prefers-color') as 'dark' | 'light')
-  }, [color])
-
-  if (!color) return null
+  const systemColor = useColorScheme();
 
   return (
-    <ConfigProvider theme={{ mode: themeMode }}>
+    <ConfigProvider theme={{ mode: color === 'auto' ? systemColor :  color }}>
       <Layout {...props}>{children}</Layout>
     </ConfigProvider>
   );
