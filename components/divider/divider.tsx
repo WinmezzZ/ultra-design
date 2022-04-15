d83c2aff1e5871159b3dde3f5bd660817d446fa6
Winme <winme0308@gmail.com>
2022-04-15 23:35:13 +0800

    chore: add Divider vertical class

--- components/divider/divider.tsx
+++ components/divider/divider.tsx
@@ -1,6 +1,7 @@
 import { dividerStyles } from './divider-styles';
 import React from 'react';
 import { useMergeProps } from '../utils/mergeProps';
+import clsx from 'clsx';
 
 export type DividerType = 'primary' | 'dashed' | 'text' | 'default';
 
@@ -23,7 +24,7 @@ const Divider: React.FC<DividerProps> = p => {
   const { vertical } = props;
 
   return (
-    <div css={dividerStyles(props)} className="ultra-divider">
+    <div css={dividerStyles(props)} className={clsx('ultra-divider', vertical && 'ultra-divider--vertical')}>
       {props.children && !vertical && <span className="ultra-divider__text">{props.children}</span>}
     </div>
   );
