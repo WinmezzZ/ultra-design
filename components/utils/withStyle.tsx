f661c81ac0f9626b924333696b14e626d27ec499
Winme <winme0308@gmail.com>
2022-04-29 00:22:16 +0800

    fix: withStyle HOC ref forward

--- components/utils/withStyle.tsx
+++ components/utils/withStyle.tsx
@@ -1,27 +1,26 @@
 import { css } from '@emotion/react';
-import { FC, useContext } from 'react';
+import { forwardRef, useContext } from 'react';
 import { ConfigProviderProps } from '../config-provider';
 import { ConfigContext } from '../config-provider/config-provider';
 
-const style = (props: ConfigProviderProps) => {
-  const { theme } = props;
-  const { mode } = theme;
+const style = (_props: ConfigProviderProps) => {
+  // const { theme } = props;
+  // const { mode } = theme;
 
   return css`
     * {
       box-sizing: border-box;
     }
     box-sizing: border-box;
-    color: ${theme[mode].textColor};
   `;
 };
 
-const withStyle = (Component: FC<any>) => {
-  const StyledComponent: FC = props => {
+const withStyle = <T, P>(Component: React.ComponentType<P & { ref?: React.Ref<T> }>) => {
+  const StyledComponent = forwardRef<T, P>((props, ref) => {
     const configContext = useContext<ConfigProviderProps>(ConfigContext);
 
-    return <Component css={style(configContext)} {...props} />;
-  };
+    return <Component css={style(configContext)} {...props} ref={ref} />;
+  });
 
   return StyledComponent;
 };
